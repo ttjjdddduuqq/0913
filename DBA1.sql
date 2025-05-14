9823e5bb93414d2df9d19cbe076bf89a7f3307a6
ttjjdddduuqq <tjdduq0913@gmail.com>
2025-05-14 21:01:15 +0900

    SQL 5월 14일 3일차

--- DBA1.sql
+++ DBA1.sql
@@ -1,6 +1,6 @@
 SELECT
-last_name,
-length(last_name) 
+    last_name,
+    length(last_name) 
 FROM hr.employees
 WHERE last_name LIKE 'J%'
 OR last_name LIKE 'A%'
@@ -8,8 +8,8 @@ OR last_name LIKE 'M%'
 ORDER BY last_name desc;
 
 SELECT
-last_name,
-length(last_name) 
+    last_name,
+    length(last_name) 
 FROM hr.employees
 WHERE substr(last_name,1,1) = 'J'
 OR substr(last_name,1,1) = 'A'
@@ -17,15 +17,15 @@ OR substr(last_name,1,1) = 'M'
 ORDER BY last_name desc;
 
 SELECT
-last_name,
-length(last_name) 
+    last_name,
+    length(last_name) 
 FROM hr.employees
 WHERE substr(last_name,1,1) IN ('J','A','M')
 ORDER BY last_name desc;
 
 SELECT
-last_name,
-length(last_name) 
+    last_name,
+    length(last_name) 
 FROM hr.employees
 WHERE instr(last_name,'J',1,1) = 1
 OR  instr(last_name,'A',1,1) = 1
@@ -50,8 +50,8 @@ SELECT
 FROM hr.employees;
 
 SELECT
-hire_date,
-trim(leading 0 from hire_date)
+    hire_date,
+    trim(leading 0 from hire_date)
 FROM hr.employees;
 
 SELECT
