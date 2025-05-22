8ea21f888877299e5a9860716549c5ba6cf5036f
ttjjdddduuqq <tjdduq0913@gmail.com>
2025-05-22 18:32:01 +0900

    Add files via upload

--- DBA2.sql
+++ DBA2.sql
@@ -2,11 +2,10 @@ SELECT level
 FROM dual
 CONNECT BY level <= 100;
 
-SELECT '2 * '||level||' = '||2*level "2단"
+SELECT '2 * '||level||' = '||2*level "2"
 FROM dual
 CONNECT BY level <= 9;
 
-
-SELECT dan||'*'||num||' = '||dan*num 구구단
+SELECT dan||'*'||num||' = '||dan*num
 FROM (SELECT level+1 dan FROM dual CONNECT BY level <= 8),
       (SELECT level num FROM dual CONNECT BY level <= 9);
