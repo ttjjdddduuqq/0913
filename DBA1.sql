f1ba4935bf3309dde5e7e9d9a1834b161b4cd726
ttjjdddduuqq <tjdduq0913@gmail.com>
2025-06-05 18:23:25 +0900

    Add files via upload

--- DBA1.sql
+++ DBA1.sql
@@ -1,299 +1,46 @@
 DECLARE
-    v_cnt number := 1;
+    v_name employees.last_name%TYPE;
+    v_sal employees.salary%TYPE;
+    v_date employees.hire_date%TYPE;
 BEGIN
-    LOOP
-        IF v_cnt = 4 OR v_cnt = 8 THEN
-            NULL;
-        ELSE
-            dbms_output.put_line(v_cnt);
-        END IF;
-        v_cnt := v_cnt + 1;
-        IF v_cnt > 10 then
-            EXIT;
-        END IF;
-    END LOOP;
+    SELECT last_name, salary, hire_date
+    INTO v_name, v_sal, v_date 
+    FROM hr.employees
+    WHERE employee_id = :b_id;
+    DBMS_OUTPUT.PUT_LINE('이름 : '||upper(v_name));
+    DBMS_OUTPUT.PUT_LINE('급여 : '||to_char(v_sal, 'fmL999,999.00'));
+    DBMS_OUTPUT.PUT_LINE('입사일 : '||to_char(v_date,'yyyy"년"fmmm"월"dd"일"'));
 END;
 /
 
 DECLARE
-    v_cnt number := 1;
+    v_day employees.hire_date%type;
+    v_sal_before employees.salary%type;
+    v_sal_after employees.salary%type;
+    v_years number;
 BEGIN
-    LOOP
-        IF v_cnt <> 4 AND v_cnt <> 8 THEN
-            dbms_output.put_line(v_cnt);
-        END IF;
-        v_cnt := v_cnt + 1;
-        IF v_cnt > 10 then
-            EXIT;
-        END IF;
-    END LOOP;
+    SELECT hire_date, salary
+    INTO v_day, v_sal_before
+    FROM hr.employees
+    WHERE employee_id = :b_id;
+    
+    v_years := trunc(months_between(sysdate,v_day) / 12);
+    dbms_output.put_line(:b_id||'사원의 입사일은 '||v_day||' 근속연수는 '||v_years||'년 입니다');
+    
+    IF v_years >= 20 THEN
+        UPDATE hr.employees
+        SET salary = salary*1.1
+        WHERE employee_id = :b_id;
+        
+        SELECT salary
+        INTO v_sal_after
+        FROM hr.employees
+        WHERE employee_id = :b_id;
+        dbms_output.put_line(:b_id||'사원의 이전 급여는 '||v_sal_before||', 수정된 급연는 '||v_sal_after);
+    ELSE
+         dbms_output.put_line(:b_id||'사원의 급여는 수정할 수 없습니다');
+    END IF;
+
+    ROLLBACK;
 END;
 /
-
-DECLARE
-    v_cnt number := 0;
-BEGIN
-    LOOP
-        v_cnt := v_cnt + 1;
-        CONTINUE WHEN mod(v_cnt,2) = 0;
-        dbms_output.put_line(v_cnt);
-        EXIT WHEN v_cnt = 9;
-    END LOOP;
-END;
-/
-
-DECLARE
-    i number := 1;
-BEGIN
-    LOOP
-        dbms_output.put_line('2 * '||i||' = '||2*i);
-        i := i + 1;
-        EXIT WHEN i = 10;
-    END LOOP;
-END;
-/
-
-DECLARE
-    i number := 0;
-BEGIN
-    LOOP
-        IF mod(i,2) <> 0 THEN
-            dbms_output.put_line(i);
-        END IF;
-        i := i + 1;
-        EXIT WHEN i > 10;
-    END LOOP;
-END;
-/
-
-DECLARE
-    i number := 1;
-BEGIN
-    LOOP
-        dbms_output.put_line(i);
-        i := i + 2;
-        EXIT WHEN i > 10;
-    END LOOP;
-END;
-/
-
-
-DECLARE
-    dan number := 2;
-    i number;
-BEGIN
-    LOOP
-        i := 1;
-        LOOP
-            dbms_output.put_line(dan||' * '||i||' = '||dan*i);
-            EXIT WHEN i =9;
-            i := i + 1;
-        END LOOP;
-        EXIT WHEN dan = 9;
-        dan := dan + 1;
-    END LOOP;
-END;
-/
-
-DECLARE
-dan number := 2;
-i number;
-BEGIN
-WHILE TRUE LOOP
-i := 1;
-WHILE TRUE LOOP
-dbms_output.put_line(dan||' * '||i||' = '||dan*i);
-EXIT WHEN i =9;
-i := i + 1;
-END LOOP;
-EXIT WHEN dan = 9;
-dan := dan + 1;
-END LOOP;
-END;
-/
-
-
-DECLARE
-dan number := 2;
-i number;
-BEGIN
-WHILE dan <= 9 LOOP
-i := 1;
-WHILE i <= 9 LOOP
-dbms_output.put_line(dan||' * '||i||' = '||dan*i);
-i := i + 1;
-END LOOP;
-dan := dan + 1;
-END LOOP;
-END;
-/
-
-BEGIN
-    FOR dan IN 2..9 LOOP
-        FOR i IN 1..9 LOOP
-            dbms_output.put_line(dan||' * '||i|| ' = '||dan*i);
-        END LOOP;
-    END LOOP;
-END;
-/
-
-
-
-DECLARE
-    i number := 1;
-    v_string varchar2(10);
-BEGIN
-    LOOP
-        v_string := v_string ||i;
-        EXIT WHEN i = 5;
-        i := i + 1;
-    END LOOP;
-    dbms_output.put_line(v_string);
-END;
-/
-
-DECLARE
-i number := 1;
-BEGIN
-LOOP
-dbms_output.put(i);
-EXIT WHEN i = 5;
-i := i + 1;
-END LOOP;
-dbms_output.new_line();
-END;
-/
-
-DECLARE
-i number := 10;
-BEGIN
-WHILE i <= 10 LOOP
-dbms_output.put_line(i);
-i := i - 1;
-EXIT WHEN i = 0;
-END LOOP;
-END;
-/
-
-DECLARE
-i number := 10;
-BEGIN
-WHILE i > 0 LOOP
-dbms_output.put_line(i);
-i := i - 1;
-END LOOP;
-END;
-/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
